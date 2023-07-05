2dc4af292d04ad0a734e4e291175790796191dc9
Francisco <drmalito.weid@gmail.com>
2023-07-05 19:08:37 -0400

    Estructuración de SeleccionarAsignatura.js

--- src/estudiante/SeleccionarAsignaturas.js
+++ src/estudiante/SeleccionarAsignaturas.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import { Container, Header } from "semantic-ui-react";
+import { Container, Header, Segment } from "semantic-ui-react";
 
 function SeleccionarAsignaturas() {
     return (
-        <div>
+        <div style={{ background: "#e8f4fc", height: "100vh" }}>
             <Navbar selectedButton={"SeleccionarAsignaturas"} />
             <Container>
                 <Header as="h1">Seleccionar asignaturas</Header>
+                <Segment color="blue"></Segment>
             </Container>
         </div>
     );
