a909d5a7b6dcc9bb1bbcaf422a1b8cf7830863e7
Francisco <drmalito.weid@gmail.com>
2023-07-06 11:32:39 -0400

    Estructuración completa de recuperar contraseña

--- src/ForgottenPassword.js
+++ src/ForgottenPassword.js
@@ -13,8 +13,67 @@ function ForgottenPassword() {
     const [usuario, setUsuario] = useState("");
     const handleUsuarioChange = (e) => setUsuario(e.target.value);
 
+    const [respuesta1, setRespuesta1] = useState("");
+    const [respuesta2, setRespuesta2] = useState("");
+    const [respuesta3, setRespuesta3] = useState("");
+
+    const handleRespuesta1Change = (e) => setRespuesta1(e.target.value);
+    const handleRespuesta2Change = (e) => setRespuesta2(e.target.value);
+    const handleRespuesta3Change = (e) => setRespuesta3(e.target.value);
+
+    const [preguntas, setPreguntas] = useState(["¿?", "¿?", "¿?"]);
+
+    // Validar usuario
     const handleSubmit = (e) => {
         e.preventDefault();
+        fetch("http://localhost:5000/Informacion/Preguntas/Usuario", {
+            method: "POST",
+            body: JSON.stringify({
+                usuario: usuario,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.length > 0) {
+                    console.log(data);
+                    setPreguntas([
+                        data[0].Pregunta,
+                        data[1].Pregunta,
+                        data[2].Pregunta,
+                    ]);
+                }
+            });
+    };
+
+    // Conseguir contraseña
+    const handleSubmitPreguntas = (e) => {
+        e.preventDefault();
+        console.log(respuesta1);
+        console.log(respuesta2);
+        console.log(respuesta3);
+
+        fetch("http://localhost:5000/Informacion/Contrasena/Recuperar", {
+            method: "POST",
+            body: JSON.stringify({
+                usuario,
+                respuesta1,
+                respuesta2,
+                respuesta3,
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.length > 0) {
+                    console.log(data);
+                    alert("Contraseña: " + data[0].Contrasena);
+                }
+            });
     };
 
     return (
@@ -36,20 +95,8 @@ function ForgottenPassword() {
                         <Link to="/">
                             <Button fluid>Volver</Button>
                         </Link>
-                        <Divider hidden />
 
-                        {/* <Form onSubmit={handleSubmit}>
-                            <Form.Input
-                                fluid
-                                label="Usuario"
-                                placeholder="Usuario"
-                                value={usuario}
-                                onChange={handleUsuarioChange}
-                            ></Form.Input>
-                            <Button primary fluid type="submit">
-                                Iniciar sesión
-                            </Button>
-                        </Form> */}
+                        <Divider hidden />
 
                         <Form onSubmit={handleSubmit}>
                             <Form.Input
@@ -59,11 +106,49 @@ function ForgottenPassword() {
                                 value={usuario}
                                 onChange={handleUsuarioChange}
                             />
+                            <Divider hidden />
+                            <Form.Button primary fluid type="submit">
+                                Validar
+                            </Form.Button>
                         </Form>
+
                         <Divider hidden />
-                        <Form.Button primary fluid type="submit">
-                            Validar
-                        </Form.Button>
+                        <Divider />
+                        <Divider hidden />
+
+                        <Form onSubmit={handleSubmitPreguntas}>
+                            <Form.Input
+                                fluid
+                                label={
+                                    "Pregunta de seguridad 1: " + preguntas[0]
+                                }
+                                placeholder="Respuesta"
+                                value={respuesta1}
+                                onChange={handleRespuesta1Change}
+                            />
+                            <Form.Input
+                                fluid
+                                label={
+                                    "Pregunta de seguridad 2: " + preguntas[1]
+                                }
+                                placeholder="Respuesta"
+                                value={respuesta2}
+                                onChange={handleRespuesta2Change}
+                            />
+                            <Form.Input
+                                fluid
+                                label={
+                                    "Pregunta de seguridad 3: " + preguntas[2]
+                                }
+                                placeholder="Respuesta"
+                                value={respuesta3}
+                                onChange={handleRespuesta3Change}
+                            />
+                            <Divider hidden />
+                            <Button primary fluid type="submit">
+                                Conseguir contraseña
+                            </Button>
+                        </Form>
                     </Grid.Column>
                 </Grid>
             </Container>
