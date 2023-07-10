0c1875a8ffa3f0d93b21896c6f76289b12de61f9
Francisco <drmalito.weid@gmail.com>
2023-07-09 23:55:54 -0400

    Front de editar asignatura completo

--- src/administrador/asignatura/AsignaturaEditar.js
+++ src/administrador/asignatura/AsignaturaEditar.js
@@ -1,8 +1,64 @@
-import React from "react";
-import { Form, Modal } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Form, Modal, Button } from "semantic-ui-react";
 
 function AsignaturaEditar({ isOpen, closeModal }) {
     const handleSubmit = (e) => {};
+
+    const [codigo, setCodigo] = useState("");
+    const [credito, setCredito] = useState("");
+    const [titulo, setTitulo] = useState("");
+    const [profesores, setProfesores] = useState([]);
+    const [areas, setAreas] = useState([]);
+    const [selectedProfesor, setSelectedProfesor] = useState("");
+    const [selectedArea, setSelectedArea] = useState("");
+
+    const handleCodigoChange = (e, { value }) => setCodigo(value);
+    const handleCreditoChange = (e, { value }) => setCredito(value);
+    const handleTituloChange = (e, { value }) => setTitulo(value);
+    const handleProfesorChange = (e, { value }) => setSelectedProfesor(value);
+    const handleAreaChange = (e, { value }) => setSelectedArea(value);
+
+    const optionsProfesor = [];
+    profesores.map((x) => {
+        optionsProfesor.push({ text: x.Nombre, value: x.Usuario });
+    });
+
+    const optionsArea = [];
+    areas.map((x) => {
+        optionsArea.push({ text: x.Area, value: x.ID });
+    });
+
+    useEffect(() => {
+        fetch("http://localhost:5000/Informacion/Profesores/Mostrar")
+            .then((response) => response.json())
+            .then((data) => {
+                setProfesores(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+
+        fetch("http://localhost:5000/Informacion/Areas/Mostrar")
+            .then((response) => response.json())
+            .then((data) => {
+                setAreas(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+
+        setCodigo(localStorage.getItem("codigo"));
+        setCredito(localStorage.getItem("credito"));
+        setTitulo(localStorage.getItem("titulo"));
+        setSelectedProfesor(localStorage.getItem("profesor"));
+        setSelectedArea(localStorage.getItem("area"));
+    }, [
+        localStorage.getItem("codigo"),
+        localStorage.getItem("credito"),
+        localStorage.getItem("titulo"),
+        localStorage.getItem("profesor"),
+        localStorage.getItem("area"),
+    ]);
 
     return (
         <Modal size="small" open={isOpen} onClose={closeModal}>
@@ -12,6 +68,7 @@ function AsignaturaEditar({ isOpen, closeModal }) {
                     <Form onSubmit={handleSubmit}>
                         <Form.Group widths="equal">
                             <Form.Input
+                                disabled
                                 width={5}
                                 label="Código"
                                 placeholder="Código"
@@ -38,7 +95,7 @@ function AsignaturaEditar({ isOpen, closeModal }) {
                             <Form.Select
                                 width={5}
                                 label="Profesor asignado"
-                                placeholder="Profesor asignado"
+                                placeholder={selectedProfesor}
                                 options={optionsProfesor}
                                 value={selectedProfesor}
                                 onChange={handleProfesorChange}
@@ -46,13 +103,13 @@ function AsignaturaEditar({ isOpen, closeModal }) {
                             <Form.Select
                                 width={4}
                                 label="Area"
-                                placeholder="Area"
+                                placeholder={selectedArea}
                                 options={optionsArea}
                                 value={selectedArea}
                                 onChange={handleAreaChange}
                             />
                         </Form.Group>
-                        <Button primary type="submit">
+                        <Button primary fluid type="submit">
                             Editar asignatura
                         </Button>
                     </Form>
--- src/administrador/asignatura/TablaAsignaturas.js
+++ src/administrador/asignatura/TablaAsignaturas.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { Table, Button } from "semantic-ui-react";
 import AsignaturaEliminar from "./AsignaturaEliminar";
+import AsignaturaEditar from "./AsignaturaEditar";
 
 const AsignaturasTable = ({ asignaturas }) => {
-    const handleEditar = (asignatura) => {
-        // Aquí puedes implementar la lógica para editar la asignatura
-        console.log("Editar asignatura:", asignatura);
+    const [isModalOpenEditar, setIsModalOpenEditar] = useState(false);
+    const openEditarAsignatura = () => setIsModalOpenEditar(true);
+    const closeEditarAsignatura = () => {
+        setIsModalOpenEditar(false);
+        localStorage.removeItem("codigo");
+        localStorage.removeItem("credito");
+        localStorage.removeItem("titulo");
+        localStorage.removeItem("profesor");
+        localStorage.removeItem("area");
     };
 
     const [isModalOpenEliminar, setIsModalOpenEliminar] = useState(false);
@@ -24,7 +31,8 @@ const AsignaturasTable = ({ asignaturas }) => {
                         <Table.HeaderCell>Título</Table.HeaderCell>
                         <Table.HeaderCell>Crédito</Table.HeaderCell>
                         <Table.HeaderCell>Profesor</Table.HeaderCell>
-                        <Table.HeaderCell>Acciones</Table.HeaderCell>{" "}
+                        <Table.HeaderCell>Area</Table.HeaderCell>
+                        <Table.HeaderCell>Acciones</Table.HeaderCell>
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
@@ -34,10 +42,33 @@ const AsignaturasTable = ({ asignaturas }) => {
                             <Table.Cell>{asignatura.Titulo}</Table.Cell>
                             <Table.Cell>{asignatura.Credito}</Table.Cell>
                             <Table.Cell>{asignatura.Profesor}</Table.Cell>
+                            <Table.Cell>{asignatura.Area}</Table.Cell>
                             <Table.Cell>
                                 <Button
                                     primary
-                                    onClick={() => handleEditar(asignatura)}
+                                    onClick={() => {
+                                        openEditarAsignatura();
+                                        localStorage.setItem(
+                                            "codigo",
+                                            asignatura.Codigo
+                                        );
+                                        localStorage.setItem(
+                                            "credito",
+                                            asignatura.Credito
+                                        );
+                                        localStorage.setItem(
+                                            "titulo",
+                                            asignatura.Titulo
+                                        );
+                                        localStorage.setItem(
+                                            "profesor",
+                                            asignatura.Profesor
+                                        );
+                                        localStorage.setItem(
+                                            "area",
+                                            asignatura.Area
+                                        );
+                                    }}
                                     content="Editar"
                                     size="compact"
                                 />
@@ -45,7 +76,10 @@ const AsignaturasTable = ({ asignaturas }) => {
                                     primary
                                     onClick={() => {
                                         openEliminarAsignatura();
-                                        localStorage.setItem("asignatura", asignatura.Codigo);
+                                        localStorage.setItem(
+                                            "asignatura",
+                                            asignatura.Codigo
+                                        );
                                     }}
                                     content="Eliminar"
                                     size="compact"
@@ -55,8 +89,15 @@ const AsignaturasTable = ({ asignaturas }) => {
                     ))}
                 </Table.Body>
             </Table>
-        
-        <AsignaturaEliminar isOpen={isModalOpenEliminar} closeModal={closeEliminarAsignatura} />
+
+            <AsignaturaEditar
+                isOpen={isModalOpenEditar}
+                closeModal={closeEditarAsignatura}
+            />
+            <AsignaturaEliminar
+                isOpen={isModalOpenEliminar}
+                closeModal={closeEliminarAsignatura}
+            />
         </>
     );
 };
@@ -68,7 +109,7 @@ function TablaAsignaturas() {
             .then((response) => response.json())
             .then((data) => {
                 setAsignaturas(data);
-                console.log(asignaturas);
+                console.log(data);
             })
             .catch((err) => {
                 console.log(err.message);
